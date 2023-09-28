b71c6006551b6314aa533f330a7ff89eb4bb1f34
Sahitya Aryan <sahityaaryansingh@gmail.com>
2023-09-28 17:14:22 +0530

    I have implemnted async await for the posting of the form data

--- frontend/src/app/register/registerUser/page.js
+++ frontend/src/app/register/registerUser/page.js
@@ -1,5 +1,5 @@
 'use client'
-
+// import dotenve from
 import { BiSolidUser, BiSolidPhone } from "react-icons/bi";
 import { RiLockPasswordFill } from "react-icons/ri";
 import { ToastContainer, toast } from 'react-toastify';
@@ -15,15 +15,14 @@ export default function LoginWorker(){
         cache:'no-cache'
     }
 
-
     const InvalidCred=()=>{
         toast.error('Invalid Credentials!', {
             position: toast.POSITION.TOP_RIGHT
         });
     }
+
     const handleSubmit = (e) =>{
         e.preventDefault();
-        // const form = document.
         const name = document.querySelector('#naam');
         const password = document.querySelector('#password');
         const phone = document.querySelector('#phone');
@@ -41,7 +40,7 @@ export default function LoginWorker(){
 
             config.body = JSON.stringify(Payload);
 
-            let postData = fetch('http://localhost:3021/registerUser',config);
+            let postData = fetch(`http://localhost:3021/registerUser`,config);
 
 
             postData.then((res)=>{
@@ -62,78 +61,59 @@ export default function LoginWorker(){
         else{
             InvalidCred();
         }
-
-
-
-
     }
 
     return(
-
 
         <>
             <ToastContainer/>
 
                 <div >
-                <div className={'w-[100vw] h-[100vh] flex justify-center items-center'}>
-                    <div className={'w-[60%] h-[70%] flex  items-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] border border-solid  rounded-2xl '}>
+                <div className={'w-[100vw] h-[100vh] flex justify-center items-center bg-indigo-500 md:bg-white '}>
+                    <div className={'w-[80%] sm:w-[60%] h-[90%] md:h-[95%] md:w-[75%] lg:h-[80%] lg:w-[70%] flex-col md:flex md:flex-row items-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] border border-solid  rounded-2xl '}>
 
-                        <div className={'w-[50%] h-[100%]'}>
+                        <div className={' w-[100%] md:w-[50%] h-[100%] bg-white border md:border-0 border-solid rounded-2xl '}>
                             <p className={'w-[100%] text-2xl font-black text-center relative top-[3rem]'}>Sign Up</p>
 
                             <form className={'w-[100%] relative top-[3rem]'} onSubmit={handleSubmit}>
 
-                                <div className={'h-[1.7rem] w-[75%] m-7 flex border-b-2 border-solid border-black  '}>
+                                <div className={'h-[15%] w-[75%] mx-auto my-2 md:m-7 flex border-b-2 border-solid  border-black  '}>
                                     <label htmlFor={'naam'}  className={' my-auto mx-2 pb-2'}><BiSolidUser /></label>
-                                    <input type={'text'} className={'mx-2 p-2 pb-3 focus:outline-none  '} placeholder={'Name'} id={'naam'} name={'naam'} required autoComplete={'on'}></input>
+                                    <input type={'text'} className={'mx-2  w-[100%] p-2 pb-3 focus:outline-none  '} placeholder={'Name'} id={'naam'} name={'naam'} required autoComplete={'on'}></input>
                                 </div>
 
 
 
                                 {/*verify your aadhar*/}
-                                <div className={'h-[1.7rem] m-7 w-[100%] my-2 flex  justify-between'} >
-                                    <div className={'h-[1.7rem] w-[75%] flex border-b-2 border-solid border-black  '}>
+                                <div className={'h-[15%] w-[100%] mx-auto my-2 md:m-7 flex md:flex-row flex-col md:justify-between '} >
+                                    <div className={'h-[100%]  w-[75%] mx-auto flex border-b-2 border-solid border-black  '}>
                                         <label htmlFor={'phone'} className={' my-auto mx-2 pb-2'}><BiSolidPhone /></label>
-                                        <input type={'number'} className={'mx-2 pb-2 focus:outline-none'} required placeholder={'Phone'} id={'phone'} name={'phone'} maxLength={10}></input>
+                                        <input type={'number'} className={' w-[100%] mx-2 p-2 pb-3 focus:outline-none '} required placeholder={'Phone'} id={'phone'} name={'phone'} maxLength={10}></input>
                                     </div>
 
-                                    <button className={"w-[25%] h-[1.9rem] mb-2 flex justify-center items-center text-md py-auto rounded px-3 mx-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900  hover:border-indigo-500 hover:bg-gradient-to-r from-sky-500 to-indigo-500 text-white"}>
+                                    <button className={" w-[70%] md:w-[25%] h-auto md:h-[2rem] mb-2 md:flex md:justify-center items-center text-md py-2 md:py-auto rounded px-3 mx-auto my-2 md:mx-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900  hover:border-indigo-500 hover:bg-gradient-to-r from-sky-500 to-indigo-500 text-white"}>
                                         Verify
                                     </button>
                                 </div>
 
-                                <div className={'h-[1.7rem] w-[75%] m-7 flex border-b-2 border-solid border-black  '}>
+                                <div className={'h-[15%] w-[75%] mx-auto my-2 md:m-7 flex border-b-2 border-solid border-black  '}>
                                     <label htmlFor={'password'}  className={' my-auto mx-2 pb-2'} ><RiLockPasswordFill /></label>
-                                    <input type={'password'}  className={'mx-2 p-2 pb-3 focus:outline-none  '} autoComplete={'off'} placeholder={'Enter Password'} id={'password'} name={'password'} required ></input>
+                                    <input type={'password'}  className={'mx-2  w-[100%] p-2 pb-3 focus:outline-none  '} autoComplete={'off'} placeholder={'Enter Password'} id={'password'} name={'password'} required ></input>
                                 </div>
 
-                                <div className={'h-[1.7rem] w-[75%] m-7 flex border-b-2 border-solid border-black  '}>
+                                <div className={'h-[15%] w-[75%] mx-auto my-2 md:m-7 flex border-b-2 border-solid border-black  '}>
                                     <label htmlFor={'c_password'} className={' my-auto mx-2 pb-2'} ><RiLockPasswordFill /></label>
-                                    <input type={'password'}  className={'mx-2 p-2 pb-3 focus:outline-none  '} autoComplete={'off'} placeholder={'Confirm Password'} id={'c_password'} name={'c_password'} required></input>
+                                    <input type={'password'}  className={'mx-2  w-[100%] p-2 pb-3 focus:outline-none  '} autoComplete={'off'} placeholder={'Confirm Password'} id={'c_password'} name={'c_password'} required></input>
                                 </div>
 
-                                <div className={'h-auto m-7 w-[90%] mx-auto text-sl my-4 text-center text-semibold text-white '}>
-                                    <input type={'submit'} className=" w-[80%] hover:cursor-pointer rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white hover:border-indigo-500 hover:bg-gradient-to-r from-sky-500 to-indigo-500" value={'Register'}></input>
+                                <div className={'h-auto mx-auto my-2 md:m-7 w-[90%] text-sl text-center text-semibold text-white '}>
+                                    <input type={'submit'} className=" w-[80%] hover:cursor-pointer rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white  " value={'Register'}></input>
 
                                 </div>
 
                             </form>
 
-                            {/* <div className='w-[100%] text-center'>
-              <p>or log in with</p>
-              <div>
-                {/* google logo */}
-                            {/* <Image
-                  src='https://cdn.worldvectorlogo.com/logos/google-g-2015.svg'
-                  width={40}
-                  height={40}
-                  className='mx-auto my-2'
-                ></Image> */}
-                            {/* </div>
-            </div> */}
                         </div>
-
-
 
                         <div className="w-[50%] h-[100%] ml-11 my-auto ">
                             <div className="hidden md:block w-[100%] h-full bg-indigo-500 py-10 px-10 border-r border-solid rounded-r-2xl">
